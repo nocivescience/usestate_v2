2aaf4f9b33347cdeb5783a2218e2198d157ec9e6
noceive <nocivescience@gmail.com>
2022-09-12 02:01:00 -0300

    trabajando con arrays

--- src/UseState/BasicUseCase.js
+++ src/UseState/BasicUseCase.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from "react";
-function BasicUseCase() {
+function BasicUseCase(props) {
   const [arr, setArr] = useState([]);
-  const [vector,setVector]=useState([])
+  const [vector,setVector]=useState([]);
   return (
     <div>
       <button onClick={() => setArr(["pilla esto bebe"])}>change arr state</button>
+      <button onClick={()=>setVector(['cayao lacra'])}></button>
       <div>
         {arr?.map((item) => {
           return <div>{item}</div>;
         })}
       </div>
+      <h2>hola: {props.nombre}</h2>
     </div>
   );
 }
