9b3d883f0e766420b4d69e48c326bb35ffd32d79
noceive <nocivescience@gmail.com>
2022-09-12 14:43:56 -0300

    terminado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import BasicUseCase from './UseState/BasicUseCase';
 
 function App() {
   return (
     <div className="App">
-     <BasicUseCase />
+     <BasicUseCase nombre='Richi' />
     </div>
   );
 }
--- src/UseState/BasicUseCase.js
+++ src/UseState/BasicUseCase.js
@@ -1,14 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 function BasicUseCase(props) {
   const [arr, setArr] = useState([]);
   const [vector,setVector]=useState([]);
+  const [count,setCount]=useState(['o'])
+  const [conteo,setConteo]=useState(0)
+  const [silaba,setSilaba]=useState([])
   return (
     <div>
       <button onClick={() => setArr(["pilla esto bebe"])}>change arr state</button>
-      <button onClick={()=>setVector(['cayao lacra'])}></button>
+      <button onClick={()=>setVector(['cayao lacra'])}>y ahora esto</button>
+      <button onClick={()=>{setCount(count+'o')}}>click me</button>
+      <button onClick={()=>{setConteo(conteo+1)}}>clickeame</button>
+      <button onClick={()=>setSilaba(['le'])}>Pinche aca</button>
       <div>
         {arr?.map((item) => {
           return <div>{item}</div>;
+        })}
+      </div>
+      <div>
+        {vector?.map((item)=>{
+          return <div>{item}</div>
+        })}
+      </div>
+      <p>here is {count}</p>
+      <p>ahora acá {conteo}</p>
+      <div>
+        {silaba?.map((sil)=>{
+          return <h1>{sil}</h1>
         })}
       </div>
       <h2>hola: {props.nombre}</h2>
